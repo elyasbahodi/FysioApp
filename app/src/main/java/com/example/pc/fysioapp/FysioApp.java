ad855e997daa9d8f31f541f3b62af71b352f9e96
kenn84 <kenneth84petersen@hotmail.com>
2018-10-13 12:24:17 +0200

    tester lige om det virker ... gå ind i java filen og se om der er en kommentar hvor der står "virker det"

--- app/src/main/java/com/example/pc/fysioapp/FysioApp.java
+++ app/src/main/java/com/example/pc/fysioapp/FysioApp.java
@@ -9,5 +9,6 @@ public class FysioApp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fysio_app);
+        // virker der 
     }
 }
